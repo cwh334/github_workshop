31b57ca64a89c3849d52d0749b02916f415dea71
Alice Huang <cwh334@nyu.edu>
2016-10-16 01:02:29 +0400

    program merged. logical error

--- battle_bot/bot.py
+++ battle_bot/bot.py
@@ -1,14 +1,4 @@
-size = 8
-
-coordinates = []
-for r in range(size):
-	for c in range(size):
-		coordinates.append([r, c])
-
-
-probability = {}
-
-def count_comb(coordinate, row, col):
+def count_sep(list_pool, coordinate, row, col):
 	# Count how many spaces there are consecutively (horizontal or vertical at a time) max 7
 	# total spaces - 3 = the number of combinations
 	count_a = 1
@@ -16,8 +6,7 @@ def count_comb(coordinate, row, col):
 	row_next = coordinate[0] + row
 	col_next = coordinate[1] + col
 	while count_a != 4:
-		print(count_a, count_b, row_next, col_next)
-		if [row_next, col_next] in coordinates:
+		if [row_next, col_next] in list_pool:
 			count_a += 1
 			row_next += row
 			col_next += col
@@ -26,22 +15,47 @@ def count_comb(coordinate, row, col):
 	row_next = coordinate[0] - row
 	col_next = coordinate[1] - col	
 	while count_b != 4:
-		print(count_a, count_b, row_next, col_next)
-		if [row_next, col_next] in coordinates:
+		if [row_next, col_next] in list_pool:
 			count_b += 1
 			row_next -= row
 			col_next -= col
 		else:
 			break
-	print("count", count_a, count_b)
 	return(count_a + count_b - 4)
 
-def create_dict(dictionary, coordinate):
-	dictionary[str(coordinate[0]) + "," + str(coordinate[1])] = count_comb(coordinate, 1, 0)
-	print(dictionary)
-	dictionary[str(coordinate[0]) + "," + str(coordinate[1])] += count_comb(coordinate, 0, 1)
-	print(dictionary)
+def count(list_pool, coordinate):
+	# Add up horizontal and vertical possibilities
+	return(count_sep(list_pool, coordinate, 1, 0) + count_sep(list_pool, coordinate, 0, 1))
 
-	return(dictionary)
-for c in coordinates:
-	print(create_dict(probability, c))
+def arrange(list_pool, result):
+	# According to the result of a guess, arrange the coordinates into corresponding lists
+	if result == " ":
+		list_pool.remove(largest_prob)
+	elif result == "x":
+		ship_loc.append(list_pool.pop(list_pool.index(largest_prob)))
+	else:
+		mine_loc.append(list_pool.pop(list_pool.index(largest_prob)))
+
+"""ship_loc = []
+mine_loc = []
+if ans != None:
+	mine_loc.append(ans)
+
+largest_prob = 0
+random.shuffle(coordinates)
+for coordinate in coordinates:
+	condition = True
+	for loc in ship_loc:
+		if abs(loc[0] - coordinate[0]) + abs(loc[1] - coordinate[1]) > 1:
+			condition = False
+	for loc in mine_loc:
+		if not(abs(coordinate[0] - ans[0]) <= 1 and abs(coordinate[1] - ans[1]) <= 1):
+			condition = False
+	if condition == True:
+			if count(coordinate) > largest_prob:
+				largest_prob = coordinate
+guess = ",".join(largest_prob)
+arrange(network.sent(guess))"""
+
+	
+
--- battle_bot/clientSimple.py
+++ battle_bot/clientSimple.py
@@ -1,11 +1,19 @@
 import network
 import random,time,os
+import bot
 
 name = input ("Please enter your name")
 
 # create boards
 dispBoard, size, shipSize, mines = network.createBoards(name, True)
 # hidboard, dispBoard, size, shipSize, mines = network.createBoards(name)
+
+""" Added code starts here to create a list of coordinates """
+coordinates = []
+for r in range(size):
+	for c in range(size):
+		coordinates.append([r, c])
+""" Ends here """
 
 while True:
 	# Opponents time to hit (waiting for opponent hit)
@@ -24,9 +32,35 @@ while True:
 	# Your turn to hit
 	# You will get back either ' ', 'x', 'M'
 
-	guess = input("enter coordinates r,c: ")
+	ship_loc = []
+	mine_loc = []
+	if ans != None:
+		mine_loc.append(ans)
 
+	largest_prob = [-2, -2]
+	random.shuffle(coordinates)
+	for coordinate in coordinates:
+		condition = True
+		for loc in ship_loc:
+			if abs(loc[0] - coordinate[0]) + abs(loc[1] - coordinate[1]) > 1:
+				condition = False
+		for loc in mine_loc:
+			if not(abs(coordinate[0] - ans[0]) <= 1 and abs(coordinate[1] - ans[1]) <= 1):
+				condition = False
+		if condition == True:
+			if bot.count(coordinates, coordinate) > bot.count(coordinates, largest_prob):
+				largest_prob = coordinate
+	guess = ",".join(list(map(str, largest_prob)))
 	ans = network.send(guess)
+	if ans == " ":
+		coordinates.remove(largest_prob)
+	elif ans == "x":
+		ship_loc.append(coordinates.pop(coordinates.index(largest_prob)))
+	else:
+		mine_loc.append(coordinates.pop(coordinates.index(largest_prob)))
+
+	# Ends here
+	
 
 	if ans == 'win':
 		break
