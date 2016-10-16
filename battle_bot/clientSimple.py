9e2b4722b5fb87211cfa6ee1c4169a05e9463bc6
Alice Huang <cwh334@nyu.edu>
2016-10-17 01:09:58 +0400

    code documented

--- battle_bot/clientSimple.py
+++ battle_bot/clientSimple.py
@@ -13,6 +13,7 @@ coordinates = []
 for r in range(size):
 	for c in range(size):
 		coordinates.append([r, c])
+# lists of found ship locations and nearby locations
 ship_loc = []
 near_loc = []
 """ Ends here """
@@ -30,50 +31,53 @@ while True:
 	elif ans != None:
 		ans = list(map(int,ans.split(',')))
 		print (ans[0],ans[1], "is nearby location of the ship")
+	# Added code to update the list of nearby locations
 		near_loc.append(ans)
-		#coordinates.remove(ans)
 	# Your turn to hit
 	# You will get back either ' ', 'x', 'M'		
 
 	largest_prob = [-2, -2]
 	random.shuffle(coordinates)
 	for coordinate in coordinates:
+		# Elimination
 		condition = True
+		# If no ship location is found yet, only compare the once near hinted nearby locations
 		if len(ship_loc) == 0:
 			for loc in near_loc:
 				if abs(coordinate[0] - loc[0]) > 1 or abs(coordinate[1] - loc[1]) > 1:
 					condition = False
+		# If one ship location is found, compare only the four locations next to it
 		elif len(ship_loc) == 1:
 			for loc in ship_loc:
 				if abs(loc[0] - coordinate[0]) + abs(loc[1] - coordinate[1]) > 1:
 					condition = False
+		# If more than 2 locations are found, compare only the two locations on the sides
 		else:
+			# horizontal
 			if ship_loc[0][0] == ship_loc[1][0]:
 				if not(coordinate[0] == ship_loc[0][0]) or\
 				not(abs(coordinate[1] - ship_loc[0][1]) == 1 or\
 					abs(coordinate[1] - ship_loc[-1][1]) == 1):
 					condition = False
+			# vertical
 			else:
 				if not(coordinate[1] == ship_loc[0][1]) or\
 				not(abs(coordinate[0] - ship_loc[0][0]) == 1 or\
 					abs(coordinate[0] - ship_loc[-1][0]) == 1):
 					condition = False
-					
+		# After elimination, choose (one of) the locations with the most possible combinations			
 		if condition == True:
 			if bot.count(coordinates, coordinate) > bot.count(coordinates, largest_prob):
 				largest_prob = coordinate
 	guess = ",".join(list(map(str, largest_prob)))
 	ans = network.send(guess)
+	# Update lists 
 	if ans == "x":
 		ship_loc.append(largest_prob)
 		ship_down = True
 	else:
 		coordinates.remove(largest_prob)
-		
-
-
 	# Ends here
-	
 
 	if ans == 'win':
 		break
