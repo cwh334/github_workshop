6f82d5a10df036b56d47769d61301ab85f196003
Alice Huang <cwh334@nyu.edu>
2016-11-06 13:06:32 +0400

    Card game final and documented

--- lab_14_cards.py
+++ lab_14_cards.py
@@ -1,5 +1,4 @@
 from random import shuffle
-from os import system
 
 suits = ["s", "h", "d", "c"]
 ranks = []
@@ -37,9 +36,9 @@ class Card:
 			return False
 
 class Deck:
-# Each deck with 52 cards, identitical unless shuffled.
-# Default attribute full = True, with identical full deck. If full = False, it will be an empty deck.
 	def __init__(self, full = True):
+# Default attribute full = True, with identical full deck. If full = False, it will be an empty deck.
+# Each deck with 52 cards, identitical unless shuffled.
 		self.deck = []
 		if full == True:
 			for suit in suits:
@@ -55,13 +54,13 @@ class Deck:
 		shuffle(self.deck)
 
 	def distribute(self):
+# First shuffle and then distribute into two decks of 26 cards
 		a = []
 		b = []
 		self.shuffle()
 		for i in range(len(self.deck)):
 			if i % 2 == 0:
 				a.append(self.deck[i])
-				print (self.deck[i])
 			else:
 				b.append(self.deck[i])
 		deck_1 = Deck(False)
@@ -72,60 +71,66 @@ class Deck:
 
 	def __len__(self):
 		return len(self.deck)
-
+# The number of cards in the deck
 	def __getitem__(self, index):
 		return self.deck[index]
-
+# Get a specific card by index
 	def pop(self, index = 0):
+# Remove and return the first card (default) in the deck. 
 		return self.deck.pop(index)
 
 	def append(self, item):
+# Append cards to this deck
 		self.deck.append(item)
+
+	def merge(self, list):
+# Merge a deck into this existing deck
+		self.deck = self.deck + list
 
 # Create a deck for each player
 original = Deck()
 p1, p2 = original.distribute()
 
-count = 26
+counts = [26, 26]
+decks = [p1, p2]
+skip = 0
+table = []
 
 while len(p1) != 0 and len(p2) != 0:
-	print(p1[0], p2[0])
-	if count == 0:
-		p1.shuffle()
-		p2.shuffle()
-		count = 26
-	if p1[0] == p2[0]:
-		if count < 4:
-			p1.shuffle()
-			p2.shuffle()
-			count = 26
-		war_card = 4
-		step = 4
-		while p1[war_card] == p2[war_card]:
-			if len(p1) < 4
-			if count < 4:
-				p1.shuffle()
-				p2.shuffle()
-				count = 26
-			war_card += step
-			count -= 4
-		for i in range(war_card + 1):
-			if p1[war_card] > p2[war_card]:
-				p1.append(p1.pop())
-				p1.append(p2.pop())
-			else:
-				p2.append(p1.pop())
-				p2.append(p2.pop())
-				
-	else:
-		if p1[0] > p2[0]:
-			p1.append(p1.pop())
-			p1.append(p2.pop())
-		if p1[0] < p2[0]:
-			p2.append(p1.pop())
-			p2.append(p2.pop())
-		count -= 1
+# Count the number of cards left (unused). 
+# When all cards in the player's hand are used, shuffle the cards won from the game into the usable deck.
+	for i in range(len(counts)):
+		if counts[i] == 0:
+			counts[i] = len(decks[i])
+			decks[i].shuffle()
 
+	print(str(p1[0]) + str(p2[0]))
+# Each players reveals their top card in the deck on the table
+	for deck in decks:
+		table.append(deck.pop())
+# When the condition skip is on, the players are at war.
+# The cards will not be compared until the forth one.
+	if skip > 0:
+		skip -= 1
+# If there is a tie. The war condition is on (skip)
+# The winner gets all the cards on the table
+	else:
+		if table[-2] == table[-1]:
+			skip = 3
+			print("Start of skips")
+		elif table[-2] > table[-1]:
+			p1.merge(table)
+			table = []
+		else:			
+			p2.merge(table)
+			table = []
+# Cards used
+	for count in counts:
+		count -= 1
+# Print temporary results after each round
+	print(len(p1), len(p2))
+
+# Whne one player loses all the cards, the player loses
 if len(p1) == 0:
 	print("Player 2 wins.")
 elif len(p2) == 0:
