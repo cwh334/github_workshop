87e046269cf32e377e72f53e1767359475442368
Alice Huang <cwh334@nyu.edu>
2016-11-15 23:02:41 +0400

    Completed and documented

--- slide_puzzle.py
+++ slide_puzzle.py
@@ -2,6 +2,8 @@ import random, os
 
 class Tile:
 	def __init__(self, value, r, c):
+		# value shows the sequence in which tiles should be in. 
+		# r, c are coordinates of current locations
 		self.value = value
 		self.r = r
 		self.c = c
@@ -10,18 +12,22 @@ class Tile:
 
 class Puzzle:
 	def __init__(self, size_row = 4, size_col = 4):
+		# specify board size
 		self.size_row = size_row
 		self.size_col = size_col
+		# create a list of tiles in the puzzle
 		self.board = []
 		value = 1
 		for row in range(self.size_row):
 			for col in range(self.size_col):
 				self.board.append(Tile(value, row, col))
 				value += 1
-		
+		# the last tile should be blank and store into another attribute self.empty
 		self.board[-1].value = " "
 		self.empty = self.board[-1]
+		# a list of values of tiles that can be moved
 		self.neighbors = []
+		# create initial game board and play
 		self.check()
 		self.shuffle()
 		self.play()
@@ -35,27 +41,33 @@ class Puzzle:
 		return tmp
 
 	def tile_by_loc(self, r, c):
+		# search for a tile in the list using its location
 		for tile in self.board:
 			if tile.r == r and tile.c == c:
 				return tile
 
 	def tile_by_value(self, value):
+		# search for a tile in the list using its value
 		for tile in self.board:
 			if tile.value == value:
 				return tile
 
 	def swap(self, tile):
+		# swap a tile with the blanl tile
 		self.empty.value = tile.value
 		tile.value = " "
 		self.empty = tile
 
 	def check(self):
+		# create and update the list of values of tiles that can be moved
 		self.neighbors = []
 		for tile in self.board:
 			if (tile.r - self.empty.r) ** 2 + (tile.c - self.empty.c) ** 2 == 1:
 				self.neighbors.append(tile.value)
 
 	def shuffle(self, rep = 500):
+		# randomly choose tiles to swap with the blank tile to make sure the puzzle is solvable
+		# repeat as many times as specified
 		for i in range(rep):
 			rand_nei = random.choice(self.neighbors)
 			self.swap(self.tile_by_value(rand_nei))
@@ -66,12 +78,16 @@ class Puzzle:
 		while win == False:
 			os.system("clear")
 			print(self)
+			# ask player to choose a tile to move
+			# error checking: number, in board range
 			choice = input("Please choose a tile to move: ")
 			while not (choice.isdigit() and int(choice) in range(self.size_row * self.size_col)):
 				choice = input("Cannot be moved. Please choose another: ")
+			# if the chosen tile is movable, swap and update the neighbors list
 			if int(choice) in self.neighbors:
 				self.swap(self.tile_by_value(int(choice)))
 				self.check()
+			# winning condition: all the tile values match their correct locations
 			count = 0
 			for tile in self.board:
 				if tile.value == tile.r * self.size_col + (tile.c + 1):
@@ -81,9 +97,3 @@ class Puzzle:
 		if win == True:
 			print(self)
 			print("You win.")
-
-
-p = Puzzle(5, 5)
-
-
-
